597779eee05b7dde58e6cce7ad6ba741b4a59fc2
Raúl Velasco <raulvsh@gmail.com>
2023-09-18 16:37:28 +0200

    BasicJS Challenges 86-95 Folder reorganization

---
+++ BasicJavaScript/BasicJS_92_ManipulatingComplexObjects.js
@@ -0,0 +1,29 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    }
+];
+//console.log(myMusic);
+myMusic.push(
+    {
+        "artist": "Nirvana",
+        "title": "Neverming",
+        "release_year": 1991,
+        "formats": [
+            "CD",
+            "Vinyl",
+            "LP"
+        ],
+        "gold": true
+    }
+)
+
+//console.log(myMusic);
