ed26c9a8e8a44e860565ece22a5ad676a84d1d6d
Raúl Velasco <raulvsh@gmail.com>
2023-09-26 23:59:44 +0200

    RegEx Challenges 01-07

---
+++ RegularExpressions/RegEx_04_IgnoreCaseWhileMatching.js
@@ -0,0 +1,5 @@
+let myString = "freeCodeCamp";
+let fccRegex = /freeCOdeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+console.log(result)
---
+++ RegularExpressions/RegEx_07_MatchAnythingWithWildcardPeriod.js
@@ -0,0 +1,6 @@
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+
+console.log(result)
+console.log(exampleStr.match(unRegex))
