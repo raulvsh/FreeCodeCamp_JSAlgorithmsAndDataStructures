99045e55f2cddf824390abacccdcb6399eff3a27
Raúl Velasco <raulvsh@gmail.com>
2023-09-27 23:07:35 +0200

    RegEx Challenges 8-15

---
+++ RegularExpressions/RegEx_13_MatchCharactersThatOccurZeroOrMoreTimes.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/g; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+
+console.log(result)
---
+++ RegularExpressions/RegEx_15_FindOneOrMoreCriminalsInAHunt.js
@@ -0,0 +1,6 @@
+let prueba = "XXCCC"
+
+let reCriminals = /C+/g; // Change this line
+
+console.log(prueba.match(reCriminals));
+
