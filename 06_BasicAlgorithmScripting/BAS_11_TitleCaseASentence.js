9cb7cc9407203093cf947ba9fc69569f001d44b4
Raúl Velasco <raulvsh@gmail.com>
2023-10-05 22:58:36 +0200

    BAS Challenge 11

---
+++ 06_BasicAlgorithmScripting/BAS_11_TitleCaseASentence.js
@@ -0,0 +1,23 @@
+function titleCase(str) {
+    let splittedStr = str.split(' ');
+    console.log(`Splitted: ${splittedStr}`)
+    for(let i=0;i<splittedStr.length;i++){
+        console.log(`Vuelta ${i}: ${splittedStr[i]}`)
+        splittedStr[i]=splittedStr[i].charAt(0).toUpperCase()+splittedStr[i].slice(1).toLowerCase()
+
+    }
+    return splittedStr.join(" ");
+  }
+
+  /* Solución cib nao
+  function titleCase(str) {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map(val => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+      .join(" ");
+  }
+  
+  titleCase("I'm a little tea pot");*/
+
+  console.log(titleCase("I'm a little tea pot"));
