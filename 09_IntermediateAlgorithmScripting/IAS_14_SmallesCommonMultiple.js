2cc9bd78c7e154c9bc98b9d451efe3c463264960
Raúl Velasco <raulvsh@gmail.com>
2023-11-08 20:25:47 +0100

    IAS Challenge 14 lcm of a range

--- 09_IntermediateAlgorithmScripting/IAS_14_SmallesCommonMultiple.js
+++ 09_IntermediateAlgorithmScripting/IAS_14_SmallesCommonMultiple.js
@@ -1,3 +1,5 @@
+/* MÍNIMO COMÚN MÚLTIPLO DOS NÚMEROS
+
 function calcularMCD(a, b) {
   // Utiliza el algoritmo de Euclides para calcular el MCD
   while (b !== 0) {
@@ -23,3 +25,37 @@ console.log(`El MCM de ${numero1} y ${numero2} es ${mcm}`);
 
 
 console.log(smallestCommons([1,5]));
+*/
+
+
+// Function to calculate the greatest common divisor (GCD) of two numbers
+function gcd(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
+// Function to calculate the least common multiple (LCM) of two numbers
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+// Function to calculate the LCM of a range of numbers
+function smallestCommons(arr) {
+  // Sort the input array in ascending order
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  var result = arr[0];
+
+  for (var i = arr[0] + 1; i <= arr[1]; i++) {
+    result = lcm(result, i);
+  }
+
+  return result;
+}
+
+console.log(smallestCommons([1, 5])); // Output: 60
